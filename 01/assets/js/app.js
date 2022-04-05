0483a5b10658ce9bd2e2d0ceb5d71e0904b2ccc2
Lukasz Scisel <lukscidev@gmail.com>
2022-04-05 21:30:42 +0200

    01 done

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,32 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+
+console.log(typeof a, typeof b);
+
+const getNumberA = Number(a)
+const getNumberB = Number(b)
+
+let sum = Number(getNumberA + getNumberB);
+console.log(sum);
+
+let sub = Number(getNumberA - getNumberB);
+console.log(sub);
+
+let mul = Number(getNumberA * getNumberB);
+console.log(mul);
+
+let div = Number(getNumberA / getNumberB)
+console.log(div);
+
+
+const arr = [];
+arr.push(sum, sub, mul, div);
+console.log(arr);
+
+arr.forEach(check => {
+    if (check > 20) {
+        console.log(`${check} jest większe od 20`)
+    } else
+        console.log(`${check} jest mniejsze od 20`)
+})
