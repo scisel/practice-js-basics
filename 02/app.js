2585d94dc11bd702805085e9ef7892bb6fc21791
Lukasz Scisel <lukscidev@gmail.com>
2022-04-06 21:04:22 +0200

    02 done

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,37 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+// const x = parseInt(prompt('Podaj liczbę od 1 do 9!'));
 
+// if (x > 0 && x < 10) {
+
+// for (let i = 1; i < 10; i++) {
+//     console.log(x * i)
+// }
+// } else {
+//     alert('Nie wpisałeś właściwej liczby')
+// }
 
 
 /* rozwiązanie z pętlą while  */
+
+const x = parseInt(prompt('Podaj podstawę potęgowania'));
+const y = parseInt(prompt('Podaj wykładnik'));
+
+let exponentiation = function (base, index) {
+    let counter = 0;
+    let result = 1;
+    let steps = ''
+    while (counter < index) {
+        result = result * base
+        counter++
+        if (counter < index) {
+            steps = steps + `${String(base)} * ` 
+        }
+        else steps = steps + base
+    }
+    steps = steps + ' = ' + result
+    console.log(steps)
+    return result
+}
+
+console.log(exponentiation(x, y))
